902dfe0fb8b0af0450c3e7af0022d490d01dc261
omar <omarcornut@gmail.com>
2019-04-10 15:24:49 +0200

    RangeSelect/MultiSelect: Fix so that shift+arrow when landing on an item that doesn't hold multi-select data doesn't trigger an unbounded range-select that ends up selecting everything until the end.

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -6761,6 +6761,9 @@ const ImGuiTypingSelectRequest* ImGui::GetTypingSelectRequest(ImGuiTypingSelectF
 // - MultiSelectItemHeader() [Internal]
 // - MultiSelectItemFooter() [Internal]
 //-------------------------------------------------------------------------
+// FIXME: Shift+click on an item that has no multi-select data could treat selection the same as the last item with such data?
+// The problem is that this may conflict with other behaviors of those items?
+//-------------------------------------------------------------------------
 
 ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void* range_ref, bool range_ref_is_selected)
 {
@@ -6827,8 +6830,9 @@ ImGuiMultiSelectData* ImGui::EndMultiSelect()
 void ImGui::SetNextItemMultiSelectData(void* item_data)
 {
     ImGuiContext& g = *GImGui;
+    IM_ASSERT(g.MultiSelectScopeId != 0);
     g.NextItemData.MultiSelectData = item_data;
-    g.NextItemData.MultiSelectDataIsSet = true;
+    g.NextItemData.MultiSelectScopeId = g.MultiSelectScopeId;
 }
 
 void ImGui::MultiSelectItemHeader(ImGuiID id, bool* p_selected)
@@ -6836,7 +6840,7 @@ void ImGui::MultiSelectItemHeader(ImGuiID id, bool* p_selected)
     ImGuiContext& g = *GImGui;
     ImGuiMultiSelectState* ms = &g.MultiSelectState;
 
-    IM_ASSERT(g.NextItemData.MultiSelectDataIsSet && "Forgot to call SetNextItemMultiSelectData() prior to item, required in BeginMultiSelect()/EndMultiSelect() scope");
+    IM_ASSERT(g.NextItemData.MultiSelectScopeId == g.MultiSelectScopeId && "Forgot to call SetNextItemMultiSelectData() prior to item, required in BeginMultiSelect()/EndMultiSelect() scope");
     void* item_data = g.NextItemData.MultiSelectData;
 
     // Apply Clear/SelectAll requests requested by BeginMultiSelect().
@@ -6877,7 +6881,7 @@ void ImGui::MultiSelectItemFooter(ImGuiID id, bool* p_selected, bool* p_pressed)
     ImGuiMultiSelectState* ms = &g.MultiSelectState;
 
     void* item_data = g.NextItemData.MultiSelectData;
-    g.NextItemData.MultiSelectDataIsSet = false;
+    g.NextItemData.MultiSelectScopeId = 0;
 
     bool selected = *p_selected;
     bool pressed = *p_pressed;
